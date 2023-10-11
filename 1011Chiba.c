71c5c55be37bc511d3fd164b0238f00396a51548
qt3g <140607840+qt3g@users.noreply.github.com>
2023-10-11 15:25:19 +0900

    Update 1011Chiba.c

--- 1011Chiba.c
+++ 1011Chiba.c
@@ -1,25 +1,35 @@
-/* PAE04_smp (2022)
+/* PAE04_ex
 -----------------------------------------------------------------
 「プログラミング応用演習」 連結リストとグラフ構造(1)
-サンプルプログラム
+演習プログラム
 -----------------------------------------------------------------
+??を含むコメントを適切なプログラムコードで置き換える
 */
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
+#define EX04_2  /* _の後ろの数字を変えれば対応した関数を使える */
+                /* 現状では，関数ex04_02を使う．*/
+                /* #define EX04_3に変更すれば，関数ex04_03を使う．*/
+
 //-----------------------------------------------------------
 #define LINE_SIZE 80    // グラフデータファイルの1行の最大文字数
+#define MAX_NODE 10     // ノードの数の最大値
 #define OK 0
 #define NG -1
 
+int AM[MAX_NODE][MAX_NODE];     // 隣接行列
 
+
+#ifdef EX04_2
 //---------------------------------------------------------------
-// 1行ずつ読んでファイルの内容を確認する
-int read_graph_1()
+int read_graph_2()
 {
     char    in_file_name[80];
-    char    line[LINE_SIZE];
     FILE    *fp;
+    int     num_node;
+    char    node_name[MAX_NODE];
+    int     cc;
 
     printf("input file name = ");
     scanf("%s", in_file_name);
@@ -30,38 +40,181 @@ int read_graph_1()
         return NG;
     }
 
-    while (fgets(line, LINE_SIZE, fp) != NULL) {
-        puts(line);     // 読み込んだ行をそのまま表示
+    fscanf(fp, "%d\n", &num_node);      // ノードの数を読み込んで表示
+    printf("number of nodes = %d\n", num_node);
+
+    // ノードの記号を読み込む
+    for (int i = 0; i < num_node; i += 1) {
+        char c;
+        fscanf(fp, "%c ", &c);
+        node_name[i] = c;
     }
-    // →グラフ構造のデータを読めるように詳細化
+
+    printf("node name: ");
+
+    for (int i = 0; i < num_node; i += 1) {
+        printf("%c ", node_name[i]);
+    }
+
+    printf("\n");
+
+    // 各辺のデータを読み込む
+    while (1) {
+        char    n1, n2;
+        int     w;
+
+        cc = fscanf(fp, /*??*/ );   // ヒント：下のprintfと同じ書式
+        if (cc == EOF) {  // ファイルの終了
+            return OK;
+        }
+
+        printf("%c %c %d\n", n1, n2, w);
+        // →隣接行列と隣接リスト作成するように詳細化
+    }
 
     return OK;
 }
 
 //-----------------------------------------------------------
-void ex04_01()        // 演習4(1)
+void ex04_02()
 {
     int     cc;
 
-    printf("read_graph_1\n");
+    printf("read_graph_2\n");
 
-    cc = read_graph_1();
+    cc = read_graph_2();
     if (cc != 0) {
         fprintf(stderr, "read error\n");
         return;
     }
 }
+#endif        // EX04_2
+
+#ifdef EX04_3
+//-----------------------------------------------------------
+int v2n(char v, char node_name[], int num_node)
+{
+    int     i;
+
+    for( i=0; /*??ループ条件*/ ; i+=1) {
+        if (node_name[i] == /*??*/) {    /* node_nameにvがあればその番号を戻り値とする */
+            return /*??*/;
+        }
+    }
+
+    return -1;    // node_nameにvがない場合は，-1を戻り値とする．
+}
+
+//------------------------
+int read_graph_3()
+{
+    char    in_file_name[80];
+    FILE    *fp;
+    int     num_node;
+    char    node_name[MAX_NODE];
+    int     cc;
+
+    printf("input file name = ");
+    scanf("%s", in_file_name);
+
+    fp = fopen(in_file_name, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Cannot open file %s\n", in_file_name);
+        return NG;
+    }
+
+    fscanf(fp, "%d\n", &num_node);  // ノードの数を読む
+    printf("number of nodes = %d\n", num_node);
+
+    // ノードの記号を読み込む
+    for (int i = 0; i < num_node; i += 1) {
+        char v;
+        fscanf(fp, "%c ", &v);
+        node_name[i] = v;
+    }
+
+    printf("node name\n");
+
+    for (int i = 0; i < num_node; i += 1) {
+        printf("%c ", node_name[i]);
+    }
+
+    printf("\n");
+
+    // 隣接行列を初期化する
+    for (int i = 0; i < num_node; i += 1) {
+        for (int j = 0; j < num_node; j += 1) {
+            AM[i][j] = 0;
+        }
+    }
+
+    // 各辺のデータを読み込む
+    while (1) {
+        char    v1, v2;
+        int     w;
+        int     n1, n2;
+
+        cc = fscanf(fp, "%c %c %d\n", &v1, &v2, &w);
+        if (cc == EOF) {
+            break;
+        }
+        
+        // ノードの記号（文字）を番号（数値）に変換する
+        n1 = v2n(v1, node_name, num_node);
+        n2 = v2n(v2, node_name, num_node);
+        // printf("%d %d %d\n", n1, n2, w);/*!!*/
+
+        // 隣接行列に値を設定する．
+        AM[/*??*/][/*??*/] = w;
+        AM[/*??*/][/*??*/] = w;
+    }
+
+    // 隣接行列を表示する
+    for (int i = 0; i < num_node; i += 1) {
+        for (int j = 0; j < num_node; j += 1) {
+            printf("%d ", AM[i][j] );
+        }
+
+        printf("\n");
+    }
+
+    return OK;
+}
+
+//-----------------------------------------------------------
+void ex04_03()
+{
+    int     cc;
+
+    printf("read_graph_3\n");
+
+    cc = read_graph_3();
+    if (cc != 0) {
+        fprintf(stderr, "read error\n");
+        return;
+    }
+}
+#endif        // EX04_3
 
 //-----------------------------------------------------------
 int main(void)
 {
-    ex04_01();
+#ifdef EX04_2
+    ex04_02();
+#endif
+#ifdef EX04_3
+    ex04_03();
+#endif
 
     return 0;
 }
 
 /*実行結果を貼り付ける
-演習4(1)
+
+演習4(2)
+
+
+演習4(3)
 
 
 */
