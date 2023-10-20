db90217c4d73253681b2a08030fd622f2a45a7c0
qt3g <140607840+qt3g@users.noreply.github.com>
2023-10-20 13:37:29 +0900

    Update 20231018.c

--- 20231018.c
+++ 20231018.c
@@ -11,8 +11,8 @@
 #define ID "19A5999"
 #define NAME "工大次郎"
 */
-#define ID      "/*?? 学生番号 */"
-#define NAME    "/*?? 氏名 */"
+#define ID      "122I066"
+#define NAME    "小島崇彰"
 
 #include "stdio.h"
 
@@ -37,19 +37,19 @@ int sptree[MAX_SIZE][MAX_SIZE];
 /*------------------------------------------------------------
 連結グラフの隣接行列を読み込み，weightを初期化する．
 */
-int read_matrix(const char *fn )
+int read_matrix(const char* fn)
 {
-    FILE    *fp;
+    FILE* fp;
     int     i, j, c;
 
     fp = fopen(fn, "r");
     if (fp == NULL)     return -1;
 
-	/* ファイルから読み取った値で，weightを初期化する */
-    for(i=0 ;i<MAX_SIZE;i+=1) {
-        for(j=0; j<MAX_SIZE; j+=1) {
-            fscanf(fp, /*??*/, /*??*/);
-            weight[/*??*/][/*??*/] = c;
+    /* ファイルから読み取った値で，weightを初期化する */
+    for (i = 0; i < MAX_SIZE; i += 1) {
+        for (j = 0; j < MAX_SIZE; j += 1) {
+            fscanf(fp,"%d", &weight[i][j]);
+            weight[i][j] = c;
         }
     }
 
@@ -67,7 +67,7 @@ int main()
     int used[MAX_SIZE] = { 0, 0, 0, 0, 0, 0, 0 };
     int total_cost;     // 全域最小木の辺のコストの和
 
-    int k,  n;
+    int k, n;
     int p1, p2;
     int m1, m2;
     int i, j;
@@ -79,28 +79,28 @@ int main()
 
 
 
-	/* 連結グラフの隣接行列をファイル am.txt から読み込む */
+    /* 連結グラフの隣接行列をファイル am.txt から読み込む */
     read_matrix("am.txt");
 
     total_cost = 0;
-    used[0] = /* 頂点0を処理済みに設定する */;
+    used[0] = 1/* 頂点0を処理済みに設定する */;
 
-    for(n = 1; n < MAX_SIZE; n += 1) {  // 未処理の頂点がなくなるまで繰り返す
+    for (n = 1; n < MAX_SIZE; n += 1) {  // 未処理の頂点がなくなるまで繰り返す
         //!printf("================\nn=%d\n", n);
 
         m1 = INF;
-        for(int i=0; i<(MAX_SIZE-1); i+=1) { // 未処理の頂点を探す
-            if(/*?? 頂点iが処理済の条件*/) {
+        for (int i = 0; i < (MAX_SIZE - 1); i += 1) { // 未処理の頂点を探す
+            if (used[i]==1/*?? 頂点iが処理済の条件*/) {
                 //!printf("i=%d>>>>\n", i);
 
                 m2 = INF;
-                for(int j=0; j<MAX_SIZE; j+=1) {      //すべての頂点の中で未処理の頂点を探す
-                    int w = /*??*/;     // wに 辺(i, j)の重み（コスト）の値を設定
+                for (int j = 0; j < MAX_SIZE; j += 1) {      //すべての頂点の中で未処理の頂点を探す
+                    int w = total_cost;     // wに 辺(i, j)の重み（コスト）の値を設定
 
-                    if( /*?? 頂点jが未処理，かつ，頂点(i,j)が接続されている条件*/ ) {
+                    if (used[j]==0 && used[i]==used[j] /*?? 頂点jが未処理，かつ，頂点(i,j)が接続されている条件*/) {
                         //!printf("  j=%d : w(%d,%d)=%d", j, i, j,w);
 
-                        if(m2 > w) {    // 新しい最小コストwが見つかった
+                        if (m2 > w) {    // 新しい最小コストwが見つかった
                             /*??*/;     // m2の値を更新
                             /*??*/;     // kの値を更新
                             //!printf(" - k=%d, m2=%d updated.", k, m2);
@@ -110,7 +110,7 @@ int main()
                     }
                 }
 
-                if((m2 < INF) && /*??*/) {    // m2が見つかり，それまでのm1より小さければ
+                if ((m2 < INF) && m2 < m1) { // m2が見つかり，それまでのm1より小さければ
                     m1 = m2;
                     p1 = i;
                     p2 = k;
@@ -127,8 +127,8 @@ int main()
         // ここまでで，頂点p1, p2が接続されており，その重みがm1となっている．
 
         // 最小全域木の隣接行列の設定
-        sptree[/*??*/][/*??*/] = m1;
-        sptree[/*??*/][/*??*/] = m1;
+        sptree[i][j] = m1;
+        sptree[i][j] = m1;
 
         total_cost += m1;   // 全域最小木の辺のコストの和の更新
     }
@@ -136,8 +136,8 @@ int main()
     printf("total cost = %d\n", total_cost);
 
     // 最小全域木の隣接行列sptreeの表示
-    for(i = 0; i < MAX_SIZE; i += 1) {
-        for(j = 0; j < MAX_SIZE; j += 1) {
+    for (i = 0; i < MAX_SIZE; i += 1) {
+        for (j = 0; j < MAX_SIZE; j += 1) {
             printf("%d ", sptree[i][j]);
         }
 
@@ -158,7 +158,6 @@ int main()
 
 //=========================================================
 /* 共同作業者がいれば，以下にコメントとして学生番号と氏名を示すこと．
-122I066 小島　崇彰
+
 
 */
-
